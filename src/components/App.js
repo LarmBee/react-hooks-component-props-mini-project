787758c8b0d0618b4ce7a67f5aab80e1bf47e92d
Brandon <brandonkanute@gmail.com>
2022-05-03 16:26:14 +0300

    Completed Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,17 @@
 import React from "react";
-import blogData from "../data/blog";
-
-console.log(blogData);
+import NavBar from "./NavBar";
+import Home from "./Home";
+import About from "./About";
+import ProjectList from "./ProjectList";
+import user from "../data/user";
 
 function App() {
   return (
-    <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+    <div>
+      <NavBar />
+      <Home username={user.name} city={user.city} color={user.color} />
+      <About bio={user.bio} links={user.links} />
+      <ProjectList projects={user.projects} />
     </div>
   );
 }
